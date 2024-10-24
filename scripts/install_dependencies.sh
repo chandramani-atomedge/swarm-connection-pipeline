85354051cb2cfbc9b9e58d1a9f2f0b146789cb7d
chandramani-atomedge <chandramani.r@atomedgesoft.com>
2024-10-24 17:32:19 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -3,7 +3,7 @@ sudo apt-get update -y
 docker service create \
   --name aura-frontend \
   --network aura-network \
-  --constraint 'node.labels.mylabel == web-instance' \
+  --constraint 'node.labels.mylabel == worker1' \
   --publish published=80,target=80 \
   --replicas 1 \
   270514764245.dkr.ecr.us-east-1.amazonaws.com/aura-frontend:latest
@@ -11,7 +11,7 @@ docker service create \
 docker service create \
   --name aura-app \
   --hostname localhost \
-  --constraint 'node.labels.mylabel == web-instance' \
+  --constraint 'node.labels.mylabel == worker1' \
   --network aura-network \
   --publish published=8080,target=8080 \
   --replicas 1 \
@@ -20,7 +20,7 @@ docker service create \
 docker service create \
   --name aura-db \
   --network aura-network \
-  --constraint 'node.labels.mylabel == db-instance' \
+  --constraint 'node.labels.mylabel == worker2' \
   --env POSTGRES_USER=postgres \
   --env POSTGRES_PASSWORD=password \
   --env POSTGRES_DB=hrms \
