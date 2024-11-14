9f8d031bc55aecf11825dabb38d073ac8542fe2c
chandramani-atomedge <chandramani.r@atomedgesoft.com>
2024-11-14 16:07:57 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,63 +1,19 @@
+#!/bin/bash
+
+# Remove existing stack
+docker stack rm aura-stack || true
+
+# Get Docker login credentials and log in
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 270514764245.dkr.ecr.us-east-1.amazonaws.com
+
+# Retrieve the Docker tags from SSM Parameter Store
+AURA_DB_TAG=$(aws ssm get-parameter --name "database-image-tag" --region ap-south-1 --query "Parameter.Value" --output text)
+AURA_APP_TAG=$(aws ssm get-parameter --name "backend-image-tag" --region ap-south-1 --query "Parameter.Value" --output text)
+AURA_FRONTEND_TAG=$(aws ssm get-parameter --name "frontend-image-tag" --region ap-south-1 --query "Parameter.Value" --output text)
+
+# Export the variables so they are available to docker-compose
+export AURA_DB_TAG
+export AURA_APP_TAG
+export AURA_FRONTEND_TAG
+
 docker stack deploy -c /home/ubuntu/swarm_connection/docker-compose.yml --detach aura-stack
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# sudo apt-get update -y
-
-# docker service rm aura-frontend || true
-# docker service rm aura-app || true
-# docker service rm aura-db || true
-
-# docker service create \
-#   --name aura-frontend \
-#   --network aura-network \
-#   --constraint 'node.labels.mylabel == worker1' \
-#   --publish published=80,target=80 \
-#   --replicas 1 \
-#   270514764245.dkr.ecr.us-east-1.amazonaws.com/aura-frontend:latest
-
-# docker service create \
-#   --name aura-app \
-#   --hostname localhost \
-#   --constraint 'node.labels.mylabel == worker1' \
-#   --network aura-network \
-#   --publish published=8080,target=8080 \
-#   --replicas 1 \
-#   270514764245.dkr.ecr.us-east-1.amazonaws.com/aura-app:latest
-
-# docker service create \
-#   --name aura-db \
-#   --network aura-network \
-#   --constraint 'node.labels.mylabel == worker2' \
-#   --env POSTGRES_USER=postgres \
-#   --env POSTGRES_PASSWORD=password \
-#   --env POSTGRES_DB=hrms \
-#   --publish published=5432,target=5432 \
-#   --replicas 1 \
-#   270514764245.dkr.ecr.us-east-1.amazonaws.com/aura-postgres:latest
